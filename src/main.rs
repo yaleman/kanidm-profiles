78ac7cbd5a49032af2ad15dbce9af0d286f36582
James Hodgkinson <james@terminaloutcomes.com>
2023-03-05 13:35:39 +1000

    path separator

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,43 @@
 //! Quick config switcher for kanidm CLI profiles
 
-use serde::{Deserialize, Serialize};
 use std::fs::File;
 use std::io::{ErrorKind, Read, Write};
 use std::path::Path;
 
+use clap::{Parser, Subcommand};
 use console::{style, Term};
+use serde::{Deserialize, Serialize};
 const CONFIG_PATH: &str = "~/.config/kanidm";
 const PROFILE_PATH: &str = "~/.config/kanidm-profiles.toml";
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+
+    #[command(subcommand)]
+    command: Option<Commands>,
+
+    /// Profile to act on
+    #[arg(short,long)]
+    profile: Option<String>,
+}
+
+
+#[derive(Subcommand)]
+enum Commands {
+    Get {
+        /// Field to get, use JSON path references like '/radius_clients/0/secret'
+        #[arg()]
+        field: String,
+    },
+    Set {
+        /// Field to set, use JSON path references like '/radius_clients/0/secret'
+        #[arg()]
+        field: String,
+        #[arg()]
+        value: String,
+    },
+}
 
 #[derive(Debug, Deserialize, Serialize)]
 struct RadiusGroup {
@@ -115,6 +145,36 @@ impl KanidmProfiles {
     }
 }
 
+/// Handles the get command
+fn get_field_value(field: String, profile: &KanidmClientConfig) {
+    eprintln!("You asked to get field {} from {}", field, profile.name.clone());
+    let parsed_profile = serde_json::json!(profile);
+    match parsed_profile.pointer(&field) {
+        Some(val) => println!("{}", val.as_str().unwrap()),
+        None => {
+            eprintln!("Couldn't find {field:?}");
+            std::process::exit(1);
+        }
+    }
+}
+/// Handles the set command
+fn set_field_value(field: String, value: String, profile: &KanidmClientConfig) {
+    eprintln!("You asked to set field {} from {}", field, profile.name.clone());
+    let mut parsed_profile = serde_json::json!(profile);
+    match parsed_profile.pointer_mut(&field) {
+        Some(val) => {
+            println!("{}", val.as_str().unwrap());
+            let newval = serde_json::Value::from(value);
+            *val = newval;
+        },
+        None => {
+            eprintln!("Couldn't find {field:?}");
+            std::process::exit(1);
+        }
+    }
+    eprintln!("Profile:\n{:#?}", parsed_profile);
+}
+
 fn main() {
     let profiles = match KanidmProfiles::default().parse_config_profiles() {
         Ok(value) => value,
@@ -124,26 +184,65 @@ fn main() {
         }
     };
 
+    let cli = Cli::parse();
+
     if profiles.profiles.is_empty() {
         println!("No profiles specified, please create some in ~/.config/kanidm-profiles.toml");
         std::process::exit(0);
     }
 
-    let selected_profile = match dialoguer::Select::new()
-        .items(&profiles.profiles)
-        .default(0)
-        .report(true)
-        .with_prompt("Please select a new profile:")
-        .interact_on_opt(&Term::stderr())
-    {
-        Ok(value) => value,
-        Err(error) => {
-            eprintln!("Uh...? {:?}", error);
+    let profile: Option<usize> = match cli.profile.clone() {
+        None => {
+            match dialoguer::Select::new()
+                .items(&profiles.profiles)
+                .default(0)
+                .report(true)
+                .with_prompt("Please select a new profile:")
+                .interact_on_opt(&Term::stderr())
+            {
+                Ok(value) => value,
+                Err(error) => {
+                    eprintln!("Uh...? {:?}", error);
+                    std::process::exit(1);
+                }
+            }
+        }
+        Some(profile_name) => {
+            profiles.profiles.iter()
+            .enumerate().find_map(|(index, profile)| {
+                match profile.name==profile_name {
+                    true => Some(index),
+                    false => {
+                        // println!("{} didn't match {}", profile.name, profile_name);
+                        None
+                    },
+                }
+            })
+        }
+    };
+
+    let mut profile = match profile {
+        Some(val) => profiles.profiles.get(val).unwrap(),
+        None => {
+            eprintln!("Couldn't find matching profile, quitting!");
             std::process::exit(1);
         }
     };
 
-    let profile = profiles.profiles.get(selected_profile.unwrap()).unwrap();
+
+    if let Some(command) = cli.command {
+        match command {
+            Commands::Get { field } => {
+                get_field_value(field, profile);
+            },
+            Commands::Set { field, value } => {
+                set_field_value(field, value, profile);
+            },
+        };
+        std::process::exit(0);
+    }
+
+
     println!("{}", style("Applying new config").green());
 
     println!("{}", toml::to_string(profile).unwrap());
